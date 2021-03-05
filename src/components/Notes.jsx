d4170d9e9b8c4b035614cd285d8e650c5c1c9fd7
Royal-lobster <srujangs8@gmail.com>
2021-03-05 09:15:29 +0530

    fixed notes page

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import styles from "./Notes.module.css";
 export default function Notes({ id }) {
-  //title state
+  // retriving storedTitle and storedNotesContent from localstorage
   let storedTitle,
     storedNotesContent = "";
   localStorage.getItem(`Title_${id}`) == null
@@ -12,33 +12,38 @@ export default function Notes({ id }) {
     ? (storedNotesContent = "")
     : (storedNotesContent = localStorage.getItem(`Title_${id}`));
 
+  // initalising state for Notes content and title
   const [NotesContent, setNotesContent] = useState(storedNotesContent);
   const [title, setTitle] = useState(storedTitle);
+
+  // useEffect to update local storage when ever title and notesContent change
   useEffect(() => {
     localStorage.setItem(`Title_${id}`, title);
-  }, [title]);
+  }, [title, id]);
   useEffect(() => {
     localStorage.setItem(`NotesContent_${id}`, NotesContent);
-  }, [NotesContent]);
+  }, [NotesContent, id]);
 
   return (
-    <div className={styles.container}>
-      <TextareaAutosize
-        className={styles.titleBox}
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        type="text"
-        placeholder="Enter Title"
-      />
-      <br />
-      <TextareaAutosize
-        placeholder="Start Typing..."
-        minRows={10}
-        className={styles.notesBox}
-        value={NotesContent}
-        onChange={(e) => setNotesContent(e.target.value)}
-        type="text"
-      />
+    <div className={styles.container_wraper}>
+      <div className={styles.container}>
+        <TextareaAutosize
+          className={styles.titleBox}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Enter Title"
+        />
+        <br />
+        <TextareaAutosize
+          placeholder="Start Typing..."
+          minRows={10}
+          className={styles.notesBox}
+          value={NotesContent}
+          onChange={(e) => setNotesContent(e.target.value)}
+          type="text"
+        />
+      </div>
     </div>
   );
 }
