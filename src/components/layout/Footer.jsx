f76354eb77bbcdb7ef067083d2a2f8d426ead3fa
royal-lobster <srujangs8@gmail.com>
2022-01-14 18:31:11 +0530

    added animation for footer for reduced layout shift effect

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,15 +1,21 @@
+import { motion } from "framer-motion";
 import React from "react";
 import styles from "./Footer.module.css";
 function Footer() {
   return (
-    <div className={styles.container}>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ default: { duration: 0.3 } }}
+      className={styles.container}
+    >
       <h4>
         Made by{" "}
         <a className={styles.Footerlink} href="https://srujangurram.me">
           Srujan
         </a>
       </h4>
-    </div>
+    </motion.div>
   );
 }
 
